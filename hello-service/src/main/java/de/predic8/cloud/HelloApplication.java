3a0aa342109023d60936832f66afc52d72be3406
helpermethod <weiler@predic8.de>
2016-09-07 15:02:15 +0200

    Add Feign RestTemplate example

--- hello-service/src/main/java/de/predic8/cloud/HelloApplication.java
+++ hello-service/src/main/java/de/predic8/cloud/HelloApplication.java
@@ -3,6 +3,7 @@ package de.predic8.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 public class HelloApplication {
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- hello-service/src/main/java/de/predic8/cloud/HelloController.java
+++ hello-service/src/main/java/de/predic8/cloud/HelloController.java
@@ -1,15 +1,10 @@
 package de.predic8.cloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 /**
  * @author Oliver Weiler (weiler@predic8.de)
@@ -17,19 +12,12 @@ import java.net.URI;
 @RestController
 public class HelloController {
 	@Autowired
-	private DiscoveryClient discoveryClient;
-	@Autowired
 	private RestTemplate restTemplate;
 
 	@RequestMapping("/greetings/{shortName}")
 	public String hello(@PathVariable("shortName") String shortName) {
-		URI uri = UriComponentsBuilder.fromUri(discoveryClient.getInstances("name-service").get(0).getUri())
-				.path("/names/{shortName}")
-				.buildAndExpand(shortName)
-				.toUri();
+		String fullName = restTemplate.getForObject("http://name-service/names/{shortName}", String.class, shortName);
 
-		ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
-
-		return "Hello " + response.getBody() + "!";
+		return "Hello " + fullName + "!";
 	}
 }
